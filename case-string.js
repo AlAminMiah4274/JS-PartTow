0bf64db466ee7a4010253b534b8d8e8bd543e0a2
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-08-28 10:50:09 +0600

    first commit

---
+++ case-string.js
@@ -0,0 +1,10 @@
+const userName = 'blackPink';
+const userInput = 'blackPinK';
+const myName = 'Al Amin Miah';
+console.log(myName.toUpperCase());
+if (userName.toLowerCase() === userInput.toLowerCase()) {
+    console.log('valid user.');
+}
+else {
+    console.log('invalid user.');
+}
---
+++ search.js
@@ -0,0 +1,9 @@
+const lyrics = 'Ei obelay tomari akashe nirob vaposhe vese jay. Tobu ei deyaler sorire raat jaga joto sukh';
+
+
+// const doesExist = lyrics.includes('obelay');
+
+const searchExist = 'joto sukh';
+const result = lyrics.includes(searchExist);
+
+console.log(result);
