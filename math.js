a91ded17dab51a1490f8205731b52a685859ab65
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-08-29 12:49:32 +0600

    about math.abs done

--- math.js
+++ math.js
@@ -1,2 +1,12 @@
 const result = Math.pow(5, 2);
-console.log(result);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 40;
+const gap = Math.abs(num1 - num2);
+if (gap < 5) {
+    console.log('You guys can be friend.');
+}
+else {
+    console.log('You guys stay apart.');
+}
