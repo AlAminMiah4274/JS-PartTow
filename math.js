3df562cce4c4f92c1a7f5b5f9f31f0f251c83dde
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-08-30 12:40:23 +0600

    completely math done

--- math.js
+++ math.js
@@ -1,12 +1,39 @@
 const result = Math.pow(5, 2);
 // console.log(result);
 
+// *********** //
+
 const num1 = 25;
 const num2 = 40;
 const gap = Math.abs(num1 - num2);
 if (gap < 5) {
-    console.log('You guys can be friend.');
+    // console.log('You guys can be friend.');
 }
 else {
-    console.log('You guys stay apart.');
+    // console.log('You guys stay apart.');
+}
+
+// ********* //
+
+const number = 3.85;
+const fullNumber = Math.round(number);
+// console.log(fullNumber);
+
+const number1 = Math.ceil(3.15222);
+// console.log(number1);
+
+const number2 = Math.floor(3.50021);
+// console.log(number2);
+
+// console.log(Math.random());
+
+const random = Math.round(Math.random()*100);
+// console.log(random);
+
+const anyNum = Math.random()*6;
+// console.log(anyNum);
+
+for(let i=0; i<=20; i++){
+    const chhokka = Math.round(Math.random()*6);
+    console.log(chhokka);
 }
