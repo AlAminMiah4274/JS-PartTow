0e0dcfaec289851e6d9121dca0cfd729e6262b0c
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-08-30 18:17:52 +0600

    done

---
+++ max3.js
@@ -0,0 +1,13 @@
+const jim = 85;
+const dela = 97;
+const chinku = 77;
+
+if (jim > dela && jim > chinku) {
+    console.log('jim will get the cake.');
+}
+else if (dela > jim && dela > chinku) {
+    console.log('Dela eat the cake in this bela.');
+}
+else {
+    console.log('Chinku will gate the cake.');
+}
