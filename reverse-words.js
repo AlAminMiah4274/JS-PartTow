d88882e4eee0532eab010c19098a892a6dad62a6
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-09-02 08:31:10 +0600

    done

--- reverse-words.js
+++ reverse-words.js
@@ -1,17 +1,32 @@
-function reverseWords(str) {
-    const word = myString.split(' ');
+// function reverseWords(str) {
+//     const word = myString.split(' ');
+//     let result = [];
+//     for (let i = word.length - 1; i >= 0; i--) {
+//         const element = word[i];
+//         result.push(element);
+//         // console.log(result);
+//     }
+//     const reversed = result.join(' ');
+//     return reversed;
+// }
+
+// const myString = 'I am a web developer';
+// const myResult = reverseWords(myString);
+// console.log(myResult);
+
+// // reverseWords(myString);
+
+function reverseWords(string) {
+    const words = string.split(' ');
     let result = [];
-    for (let i = word.length - 1; i >= 0; i--) {
-        const element = word[i];
+    for (let i = words.length - 1; i >= 0; i--) {
+        const element = words[i];
         result.push(element);
-        // console.log(result);
     }
-    const reversed = result.join(' ');
+    reversed = result.join(' ');
     return reversed;
 }
 
 const myString = 'I am a web developer';
 const myResult = reverseWords(myString);
 console.log(myResult);
-
-// reverseWords(myString);
