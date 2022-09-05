bdb73c0acec4c8b26d6ba9d74eda86268e829568
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-09-05 19:56:44 +0600

    done

--- duplicate.js
+++ duplicate.js
@@ -1,10 +1,15 @@
 function removeDuplicate(names) {
+    let unique = [];
     for (let i = 0; i < names.length; i++) {
-        let name = names[i];
-        console.log(name);
+        const name = names[i];
+        if (unique.includes(name) === false) {
+            unique.push(name);
+        }
     }
+    return unique;
 }
 
-const nameList = ['al amin', 'sabbir', 'masum', 'rakib', 'sabbir', 'masum', 'al amin', 'al amin',
-    'rakib', 'masum', 'sabbir', 'masum', 'al amin', 'rakib'];
+const nameList = ['al amin', 'masum', 'sabbir', 'rakib', 'al amin', 'rakib', 'masum', 'sabbir', 'rakib', 'masum', 'rakib',
+    'al amin', 'sabbir', 'masum', 'al amin', 'masum', 'rakib', 'al amin',];
 const result = removeDuplicate(nameList);
+console.log(result);
